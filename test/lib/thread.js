7e6e29091041d17d372590786b58d398a184cdfd
Wilson Page <wilsonpage@me.com>
2015-05-26 12:07:55 +0100

    Fix falsy return values not reaching client (fixes #35)

--- test/lib/thread.js
+++ test/lib/thread.js
@@ -8,6 +8,26 @@ try {
   threads.service('view-server')
     .method('getData', function(id) {
       return { some: 'data' };
+    })
+
+    .method('returnsFalse', function() {
+      return false;
+    })
+
+    .method('returnsEmptyString', function() {
+      return '';
+    })
+
+    .method('returnsNull', function() {
+      return null;
+    })
+
+    .method('returnsZero', function() {
+      return 0;
+    })
+
+    .method('returnsUndefined', function() {
+      return null;
     });
 
 } catch(e) {
